3e300ad18f9d109de429236e83d131f95469f5d9
Tazunebi <tazoozaalishvili@gmail.com>
2024-02-09 17:54:22 +0400

    Day 2

---
+++ Day 2/lesson2.py
@@ -0,0 +1,8 @@
+print("me gavicani")
+#mate giorgelashvili 
+#dima bakuradze
+#irakli kvinchia
+print("ras vfiqrob goaze")
+#goa aris sauketeso academia sauketeso xalxit momewona mentori misi
+#da aseve sauketeso razmis lideri myavs madloba mates 
+#rom sulyoveltvis gvexmareba
