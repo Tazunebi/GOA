23bf6915b3ff0d1c65f6e3a920e19fa60cc65d22
Tazunebi <tazoozaalishvili@gmail.com>
2024-02-19 19:27:36 +0400

    commit

--- Day 04/homeworks/lesson4.py
+++ Day 04/homeworks/lesson4.py
@@ -2,3 +2,7 @@ name = input("what is your name: ")
 lastname = input("what is your lastname: ")
 
 print("welcome",name,lastname,)
+
+
+
+
---
+++ Day 05/homeworks/lesson5.py
@@ -0,0 +1,9 @@
+num1 = int(input("please enter number1: "))
+num2 = int(input("please enter number2: "))
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1>num2)
+print(num1<num2)
